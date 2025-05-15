0ff6a1cf717b2e3d2c8fcba59593dbf0b1ebfba3
matget <matget@gmail.com>
2025-05-15 21:24:51 +0300

    Update run.sh

--- btc_bot/run.sh
+++ btc_bot/run.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 echo "🔧 Starting Bitcoin Bot add-on..."
 
-# Make sure push.sh is executable
-chmod +x /push.sh
-
 # Start your bot
 echo "📦 Running bot listener..."
 python3 /app/run.py listen
